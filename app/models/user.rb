779409d975f0ecbc64a5cf3765cbdda1e58d2045
ndiki931 <kidibrahima@gmail.com>
2020-05-29 15:43:47 +0200

    files updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
 	has_many :attendances
 	has_many :events, through: :attendances
+
+	after_create :welcome_user
+
+  	def welcome_user
+    	UserMailer.welcome_email(self).deliver_now
+  	end
+
 end
+
